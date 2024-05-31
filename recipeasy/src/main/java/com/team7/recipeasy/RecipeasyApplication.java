0553b686687f9bdfa60d916d8319320402677bfb
Matthew Burden <mdburden@uncg.edu>
2024-05-31 16:54:52 -0400

    Team7: Moved files to Spring app

---
+++ recipeasy/src/main/java/com/team7/recipeasy/RecipeasyApplication.java
@@ -0,0 +1,13 @@
+package com.team7.recipeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeasyApplication.class, args);
+	}
+
+}
