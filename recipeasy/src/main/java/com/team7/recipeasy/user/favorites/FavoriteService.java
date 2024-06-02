6606bf3763b630f96d17672f29c458465081ef17
sk98789 <s_korzekwa@uncg.edu>
2024-06-02 17:44:57 -0400

    Add getAllFavorites method to FavoriteService

--- recipeasy/src/main/java/com/team7/recipeasy/user/favorites/FavoriteService.java
+++ recipeasy/src/main/java/com/team7/recipeasy/user/favorites/FavoriteService.java
@@ -3,8 +3,16 @@ package com.team7.recipeasy.user.favorites;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class FavoriteService {
     @Autowired
     FavoriteRepository repo;
+
+    public List<Favorite> getAllFavorites(){
+        return repo.findAll();
+    }
+
+
 }
