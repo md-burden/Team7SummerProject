4710757bdd57be537ab73b3de5fa6fb4d8ff3ad1
sk98789 <s_korzekwa@uncg.edu>
2024-06-02 02:08:46 -0400

    user controller mappings

--- recipeasy/src/main/java/com/team7/recipeasy/user/UserController.java
+++ recipeasy/src/main/java/com/team7/recipeasy/user/UserController.java
@@ -1,7 +1,33 @@
 package com.team7.recipeasy.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/all")
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User findUserById(@PathVariable int id){
+        return userService.getUserById(id);
+    }
+
+    @PutMapping("/ban/{id}")
+    public void banUserById(@PathVariable int id){
+        userService.BanUserById(id);
+    }
 }
